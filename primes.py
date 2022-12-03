f26aba53df56ab99eb4394431c218487a95f5a71
Baiyu <baiyu.liu@kcl.ac.uk>
2022-12-03 21:45:46 +0000

    fin

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes < 1:
+        raise ValueError("Number of primes is 0 or negative")
+
+    list.append(2)
+
+    i = 3
+    while len(list) < number_of_primes:
+        isPrime = True
+        for j in range(i-1,1,-1):
+            if i % j == 0:       #If it is divisible by a number less than it, or a number down to 2, then it is not prime
+                isPrime = False
+                break
+        
+        if isPrime:
+            list.append(i)
+            
+        i = i+1    
+
     return list
+
+print(primes(5))
